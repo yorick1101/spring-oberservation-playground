2eb987705609100c8fa3362764bac487c2574f50
yorickopennet <yorick.yu@opennet.tw>
2024-12-12 15:53:02 +0800

    init project

---
+++ src/test/java/yorick/poc/spring/observation/SpringObservationPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package yorick.poc.spring.observation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringObservationPlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
